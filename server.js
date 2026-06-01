ce0ab46a72c8acad0f4b2543686de2769b0052ff
Fritado <fritado.com@gmail.com>
2026-06-01 13:04:48 +0530

    Fix Express 5 compatibility in server.js catch-all route

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 30006;
 app.use(express.static(path.join(__dirname, 'dist')));
 
 // Catch-all route to serve the React app for any path (client-side routing)
-app.get('*', (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
